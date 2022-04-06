4e46c303eb9f8280e1e2697642b605878d3d38d0
Malte <malte@dieheringe.de>
2022-04-06 20:49:29 +0200

    Version 1.2

--- htmlimageservice_uninstall.sh
+++ htmlimageservice_uninstall.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 
-apt install postgresql postgresql-contrib
-sudo -u postgres psql -c "SELECT version();"
-#sudo su - postgres -c "createuser postgres"
-
-
-apt install nodejs npm
-apt install git
-git clone https://github.com/Multie/documentsStorage
-cd documentsStorage/server 
-npm install
-npm install -g nodemon
-nodemon server.js
 
 
